0251b7774238de98ca1804edc7fb9f8c2287ede9
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-06 14:58:35 +0900

    1006

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
