8e294f96ac4facff90e45df9e21884b52b995663
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-09 16:25:22 +0900

    1009

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,14 +1,19 @@
 class RecordController < ApplicationController
   # 名前なしパラメーター
-  def ph1
-    @books = Book.where('publish = ? AND price >= ?', params[:publish], params[:price])
-    render 'hello/list'
-    # ?はプレイスホルダー。プレイスホルダーにセットする値は第２引数で指定。
-  end
+  # def ph1
+  #   @books = Book.where('publish = ? AND price >= ?', params[:publish], params[:price])
+  #   render 'hello/list'
+  #   # ?はプレイスホルダー。プレイスホルダーにセットする値は第２引数で指定。
+  # end
 
   # 名前ありパラメーター
   def ph1
     @books = Book.where('publish = :publish AND price >= :price', params[:publish], params[:price])
     render 'hello/list'
+  end
+
+  def not
+    @books = Book.where.not(isbn: params[:id])
+    render 'books/index'
   end
 end
